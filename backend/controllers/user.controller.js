49727aea37a4df4962fa0c236ba4ec3ac7a0fade
Zubaer-Alam <xuvaer@gmail.com>
2023-10-05 11:58:13 +0600

    Implement JWT

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,14 +1,21 @@
 const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const loginUser = async (req, res) => {
-  res.json({ msg: "login user" });
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
 };
 
+// LOGIN
+const loginUser = async (req, res) => {};
+
+// SIGNUP
 const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
