5ce36e3490d53124cd1505bfc275a256436cb13b
Zubaer-Alam <xuvaer@gmail.com>
2023-10-05 18:45:31 +0600

    ✔️Logout implemented

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
+  const { logout } = useLogout();
+  const handleClick = () => {
+    logout();
+  };
+
   return (
     <header>
       <div className="container">
-        <nav className="flex justify-end">
+        <div className="flex justify-end">
+          <button
+            onClick={handleClick}
+            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Logout
+          </button>
+        </div>
+        <nav className="flex justify-left">
           <div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
