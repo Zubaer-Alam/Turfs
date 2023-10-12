e79baf32848ca4ae7b3792c4935aff79b49b8898
Zubaer-Alam <xuvaer@gmail.com>
2023-10-12 11:58:11 +0600

    Update Bookings display

--- frontend/src/components/Booking.jsx
+++ frontend/src/components/Booking.jsx
@@ -4,11 +4,16 @@ export const Booking = ({ booking }) => {
   return (
     <div className="p-1">
       <Card>
-        <Card.Header as="h5">Turf : {booking.turfName}</Card.Header>
+        <Card.Header as="h5">Turf: {booking.turfName}</Card.Header>
         <Card.Body>
           <Card.Title>{booking.date}</Card.Title>
           <Card.Text>{booking.time}</Card.Text>
-          {/* <Button variant="outline-danger">Delete Booking</Button> */}
+          <Card.Text>Slots:</Card.Text>
+          <Card.Text>
+            {booking.dayTime && <Card.Text>{booking.dayTime}</Card.Text>}
+            {booking.nightTime && <Card.Text>{booking.nightTime}</Card.Text>}
+          </Card.Text>
+          <Button variant="outline-danger">Delete Booking</Button>
         </Card.Body>
       </Card>
     </div>
